bb79064f6c586c6025ed0d37c630807fc8ad530a
aemamonov <mamonov.aleks.e@gmail.com>
2023-10-04 23:29:48 +0300

    little improvements

---
+++ lib/entity/position_data.dart
@@ -0,0 +1,11 @@
+import 'package:just_audio/just_audio.dart';
+
+class PositionData {
+  final Duration position;
+  final PlayerState playerState;
+
+  PositionData(
+    this.position,
+    this.playerState,
+  );
+}
